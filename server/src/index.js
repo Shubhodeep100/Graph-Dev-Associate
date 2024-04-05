a232bf47a401be8b518635c343ec6b5c133d270e
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-05 13:42:57 +0530

    schema work

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const gql = require("graphql-tag");
+
+const typeDefs = gql`
+  "A track is a group of Modules that teacches about a specific topic"
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+`;
+
+module.exports = typeDefs;
