ede5958ccec23be86ee4d9d9a1d606ff6f4151ef
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-05 14:32:42 +0530

    Apollo server setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
-//TODO
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = "@apollo/server/standalone";
+const typeDefs = require("./schema");
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`Server is Running!
+    query at ${url}
+    `);
+}
+
+startApolloServer();
