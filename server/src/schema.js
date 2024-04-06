288b69134dbcba86ca7af7875d2257edd552faaf
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-06 13:25:26 +0530

    schema query

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,13 @@
 const gql = require("graphql-tag");
 
 const typeDefs = gql`
+"Query fileds are the entry points the rest of our schema"
+"Query to get tracks array of the homepage grid"
+type Query {
+tracksForHome: [Track!]!
+}
+
+
   "A track is group of modules that teaches about a specific topic"
   type Track {
     id: ID!
@@ -11,6 +18,7 @@ const typeDefs = gql`
     modulesCount: Int
   }
 
+  "Author of a complete track"
   type Author {
     id: ID!
     name: String!
