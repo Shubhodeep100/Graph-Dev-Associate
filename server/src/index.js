7133de4d0b76ed72fcd40617aecde65af00b8469
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-06 13:16:12 +0530

    git check

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ async function startApolloServer() {
   });
 
   const { url } = await startStandaloneServer(server);
-  console.log(`Server is Running!
+  console.log(`Server is a Running!
     query at ${url}
     `);
 }
